215e78d7507491573b62e8a29b4484aafcb04f80
Jen Miao <jenmiao@gmail.com>
2020-02-17 04:46:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,13 +12,18 @@ def batch_badge_creator(attendees)
 end
 
 
-def assign_rooms(attendees)
-  newArr = []
-  attendees.each_with_index do |name, i| newArr << "Hello, #{name}! You'll be assigned to room #{i+1}!"
-  newArr
-  end
-end
+# def assign_rooms(attendees)
+#   newArr = []
+#   attendees.each_with_index do |name, i| newArr << "Hello, #{name}! You'll be assigned to room #{i+1}!"
+#   newArr
+#   end
+# end
 
+def assign_rooms(speakers)
+  greet = []
+  speakers.each_with_index{ |speakers, index| greet << "Hello, #{speakers}! You'll be assigned to room #{index+1}!"}
+  return greet
+  end
 
 
 def printer(attendees)
