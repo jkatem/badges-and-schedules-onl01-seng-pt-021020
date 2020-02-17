0a32798ca39e08413743f47998612599487cc6c7
Jen Miao <jenmiao@gmail.com>
2020-02-17 04:34:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ def assign_rooms(attendees)
   newArr = []
   attendees.each_with_index do |name, i|
     puts "Hello, #{name}! You'll be assigned to room #{i+1}!"
-    newArr = 
+    newArr =
   end
 end
 
